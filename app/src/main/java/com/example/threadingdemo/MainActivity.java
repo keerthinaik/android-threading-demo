86d86adc794c9ea6879b3fb33d69623cfa94a0b8
KEERTHINAIK R <keerthinaik.r@outlook.com>
2022-03-02 23:11:40 +0530

    Added basic main and thread handlers code

--- app/src/main/java/com/example/threadingdemo/MainActivity.java
+++ app/src/main/java/com/example/threadingdemo/MainActivity.java
@@ -3,12 +3,83 @@ package com.example.threadingdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Button buttonStartThread;
+
+    private ExampleThread exampleThread;
+
+    private Handler mainHandler = new Handler();
+
+    private volatile boolean stopThread = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStartThread = findViewById(R.id.button_start_thread);
+    }
+
+    public void startThread(View view) {
+        stopThread = false;
+        exampleThread = new ExampleThread(10);
+        exampleThread.start();
+    }
+
+    public void stopThread(View view) {
+        stopThread = true;
+    }
+
+    class ExampleThread extends Thread {
+
+        private int seconds;
+
+        public ExampleThread(int seconds) {
+            this.seconds = seconds;
+        }
+
+        @Override
+        public void run() {
+            for (int i=0; i<seconds; i++) {
+                if (stopThread) {
+                    return;
+                }
+                Log.d(TAG, "startThread " + i);
+                if (i == seconds/2) {
+
+                    runOnUiThread(() -> {
+                        buttonStartThread.setText("50%");
+                    });
+
+                    /*mainHandler.post(() -> {
+                        buttonStartThread.setText("50%");
+                    });*/
+
+                    /*Handler threadHandler = new Handler(Looper.getMainLooper());
+                    threadHandler.post(() -> {
+                        buttonStartThread.setText("50%");
+                    });*/
+
+                    /*buttonStartThread.post(() -> {
+                        buttonStartThread.setText("50%");
+                    });*/
+
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
